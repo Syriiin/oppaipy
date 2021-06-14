9f63674bc57509e6688c1098993c9a09a77415f0
Samuel Cattini-Schultz <syriiins@gmail.com>
2021-06-14 16:39:13 +1000

    Fix versioning and bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fp:
 
 setuptools.setup(
     name="oppaipy",
-    version="1.0.3",
+    version="1.0.4",
     author="Syrin",
     author_email="syrin@syrin.me",
     description="A simple object-oriented python3 wrapper around the python bindings of oppai-ng",
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/Syriiin/oppaipy",
     packages=setuptools.find_packages(),
     install_requires=[
-        "oppai"
+        "oppai~=4.1"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
